eaa0ce4c18547486784f3ef38b353a3fdd2184ff
VexXzZz <121412984+VexXzZz@users.noreply.github.com>
2022-12-26 23:03:07 +0100

    Update Admin.lua

--- Features/Admin.lua
+++ Features/Admin.lua
@@ -1,6 +1,6 @@
 -- \\ If the whitelist API is down make sure to refresh before adding a whitelist! // --
 ADMIN = {
 	[3716850885] = true,
-	[3682757800] = true,
+	[2486938906] = true,
 }
 return ADMIN
