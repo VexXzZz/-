78af92ffe56161a81822e823092c3d3d189b0745
VexXzZz <121412984+VexXzZz@users.noreply.github.com>
2022-12-26 23:02:51 +0100

    Update Emoji.lua

--- Features/Emoji.lua
+++ Features/Emoji.lua
@@ -1,7 +1,7 @@
 -- \\ If the whitelist API is down make sure to refresh before adding a whitelist! // --
 local StarId = {
 	[3716850885] = true,
-	[3682757800] = true,
+	[2486938906] = true,
 }
 
 local CrownId = {}
